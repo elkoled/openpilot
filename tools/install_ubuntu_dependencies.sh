f144d3c83fff90d0848f59eb4d78f2a0189db64c
elkoled <elkoled@gmail.com>
2025-02-22 15:57:21 +0100

    refs

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -84,7 +84,7 @@ function install_ubuntu_lts_latest_requirements() {
 if [ -f "/etc/os-release" ]; then
   source /etc/os-release
   case "$VERSION_CODENAME" in
-    "jammy" | "kinetic" | "noble")
+    "jammy" | "kinetic" | "noble" | "wilma")
       install_ubuntu_lts_latest_requirements
       ;;
     *)
