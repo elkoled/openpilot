aab3ea0960cde2f0c07c1c19936c073ed530a165
elkoled <elkoled@gmail.com>
2026-09-08 15:04:43 -0700

    ci: keep the Chestnut runner to a fresh SCons build

--- openpilot/selfdrive/test/chestnut.sh
+++ openpilot/selfdrive/test/chestnut.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-test -f /data/disable_openpilot_autostart
-test "$(cat /data/params/d/IsOffroad)" = 1
-python -c 'from openpilot.common.hardware import HARDWARE; from openpilot.selfdrive.modeld.helpers import chestnut_present; assert HARDWARE.get_device_type() == "mici" and chestnut_present()'
-
-MODEL=openpilot/selfdrive/modeld/models/big_driving_tinygrad.pkl
-trap 'rm -f "$MODEL"*' EXIT
-rm -f "$MODEL.chunkmanifest"
-scons -j2 --cache-disable "$MODEL.chunkmanifest"
-test -s "$MODEL.chunkmanifest"
+TARGET=openpilot/selfdrive/modeld/models/big_driving_tinygrad.pkl.chunkmanifest
+rm -f "$TARGET"
+scons --cache-disable "$TARGET"
+test -s "$TARGET"
